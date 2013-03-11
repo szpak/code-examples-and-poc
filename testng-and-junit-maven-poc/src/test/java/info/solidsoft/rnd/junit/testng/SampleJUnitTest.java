571cbfcb9b3e5d262820ea58070f71791d134e01
Marcin Zajaczkowski <szpak@users.sourceforge.net>
2013-03-11 23:38:30 +0100

    add testng-and-junit-maven-poc

---
+++ testng-and-junit-maven-poc/src/test/java/info/solidsoft/rnd/junit/testng/SampleJUnitTest.java
@@ -0,0 +1,13 @@
+package info.solidsoft.rnd.junit.testng;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class SampleJUnitTest {
+
+    @Test
+    public void shouldWorkWithJUnitAssertion() {
+        assertTrue(true);
+    }
+}
---
+++ testng-and-junit-maven-poc/src/test/java/info/solidsoft/rnd/junit/testng/SampleTestNGTest.java
@@ -0,0 +1,13 @@
+package info.solidsoft.rnd.junit.testng;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+@Test
+public class SampleTestNGTest {
+
+    public void shouldWorkWithTestNGAssertion() {
+        assertTrue(true);
+    }
+}
