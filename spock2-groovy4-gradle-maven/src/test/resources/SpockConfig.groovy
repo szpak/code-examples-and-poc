e1c146db3a9c855d76697cd2a195c5a01cfc3d9d
Marcin Zajączkowski <148013+szpak@users.noreply.github.com>
2022-05-12 23:47:55 +0200

    [Spock2WhatsNew] Hide feature name only if executed from Idea

--- spock2-groovy4-gradle-maven/src/test/resources/SpockConfig.groovy
+++ spock2-groovy4-gradle-maven/src/test/resources/SpockConfig.groovy
@@ -1,8 +1,12 @@
+static boolean isExecutedFromIdea() {
+    return System.getProperty("java.class.path").contains("idea_rt.jar")
+}
+
 unroll {
 //    unrollByDefault true  //not needed, enabled by default
 //    validateExpressions true  //not needed, enabled by default
 
-//    defaultPattern '#featureName [#iterationIndex]'
 //    defaultPattern '#dataVariablesWithIndex'
-    includeFeatureNameForIterations false
+//    includeFeatureNameForIterations false
+    includeFeatureNameForIterations !isExecutedFromIdea()
 }
